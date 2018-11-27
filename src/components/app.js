2b1001cc1ecae4971d0ce947a823c9be03807fb7
ricardoponcedeleon <poncedeleon.richie@gmail.com>
2018-11-26 23:44:46 -0700

    hi first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React, Props, and State Deep Dive</h2>
+      </div>
+    );
+  }
+}
